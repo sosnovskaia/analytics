ee5cdc8e0f1fb1653859eab066f4a5c1bff5f87f
sosnovskaia <e-sosnovskaya@ya.ru>
2020-07-07 00:28:21 +0300

    Initial commit

--- wb_select-join.sql
+++ wb_select-join.sql
@@ -2,55 +2,85 @@ use wildberries;
 
 -- выборки
 
-SELECT * FROM users LIMIT 1;
-SELECT * FROM users WHERE id = 14; 
-SELECT firstname, lastname FROM users WHERE id = 14;
+select * from users limit 1;
+select * from users where id = 14; 
+select firstname, lastname from users where id = 14;
 
- SELECT *
-  FROM orders 
-  WHERE user_id = 11;
+ select *
+  from orders 
+  where user_id = 11;
 
  
  -- выборка по определенному критерию
- SELECT product_name FROM products
-	WHERE category_id = 8
-  AND id = (
-    SELECT id FROM products WHERE description LIKE 'Similique odit similique' limit 1
+ select product_name from products
+	where category_id = 8
+  and id = (
+    select id from products where description like 'Similique odit similique' limit 1
 );
 
 -- пользователь и персональная скидка
-SELECT 
+select 
 	firstname, 
 	lastname,
 	'city',
 	'discount' -- персональная скидка
-FROM users 
-WHERE id = 11;
+from users 
+where id = 11;
 
-SELECT 
+select 
 	firstname, 
 	lastname, 
-	(SELECT hometown FROM profiles WHERE user_id = 11) AS city,
-	(SELECT discount FROM personal_discount WHERE user_id = 11) AS discount
-FROM users 
-WHERE id = 11;
+	(select hometown from profiles where user_id = 11) as  city,
+	(select discount from personal_discount where user_id = 11) as discount
+from users 
+where id = 11;
 
 -- сколько заказов у каждого пользователя? 5 пользователей с большим количеством заказов  
-SELECT COUNT(id) AS orders_count, user_id
-	FROM orders
-	GROUP BY user_id
-	ORDER BY orders_count desc   limit 5;
+select  COUNT(id) as orders_count, user_id
+	from orders
+	group by user_id
+	order by orders_count desc limit 5;
 	
 -- оплаченные и доставленнные заказы
-SELECT * FROM orders
-WHERE payment_id = (
-	select id from payments where status LIKE 'оплата произведена' limit 1)
+select * from orders
+where payment_id = (
+	select id from payments where status like 'оплата произведена' limit 1)
 	and delivery_type_id = (
 	select id from delivery where status like 'доставлен' limit 1);
 
--- join'ы
-SELECT 
+
+alter table delivery 
+ADD column order_id BOOL default false;
+update delivery, (select @i:=0) x set order_id = @i:=@i+1;
+
+alter table payments 
+add column order_id BOOL default false;
+update payments, (select @i:=0) x set order_id = @i:=@i+1;
+alter  table payments 
+add column user_id BOOL default false;
+update payments, (select @i:=0) x set user_id = @i:=@i+1;
+
+
+-- данные 11 пользователя
+select 
 	firstname, lastname, email, phone, gender, birthday, hometown, photo
-  FROM users
-    JOIN profiles ON users.id = profiles.user_id
-  WHERE users.id = 11;
+  from users
+    join profiles on users.id = profiles.user_id
+  		where users.id = 11;
+  
+ 
+ -- количество оплаченных заказов у всех пользователей
+select firstname, lastname, COUNT(*) AS total_paymentss
+  from users
+  join payments on (users.id = payments.user_id)
+  	where payments.status = 'оплата произведена'
+ group by users.id;
+  
+
+select * from `orders`
+	where id = (select `to_user_id` from `messages` where `to_user_id`
+		in (select `target_user_id` from `friend_requests` 
+			where `initiator_user_id` = 1 and `status` = 'approved'
+union
+select `initiator_user_id` from `friend_requests` 
+	where `target_user_id` = 1 and `status` = 'approved') order by `to_user_id` asc limit 1);
